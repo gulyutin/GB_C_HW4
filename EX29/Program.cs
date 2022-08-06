fa26ee09231182733f9c3ca93483d5d72bb614cb
Gulyutin <gulyutin@gmail.com>
2022-08-06 18:35:16 +0700

    EX29

---
+++ EX29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+// массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] array = GetArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int[] GetArray (int size)
+{
+    int [] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next();
+    }    
+    return result;
+}
